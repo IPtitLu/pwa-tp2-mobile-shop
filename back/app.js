ba95a2f0d488f683386bea4ad9521f49bf6573e0
Nephyrr <lucasperez702@gmail.com>
2023-01-13 21:10:54 +0100

    login with back & front

--- back/app.js
+++ back/app.js
@@ -1,33 +1,36 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import mongoose from 'mongoose';
-import Articles from './src/controllers/Articles.js'
-import Categories from './src/controllers/Categories.js';
-import Users from './src/controllers/Users.js';
-import {Login} from './src/controllers/Login.js';
+import mongoose from "mongoose";
+import Articles from "./src/controllers/Articles.js";
+import Categories from "./src/controllers/Categories.js";
+import Users from "./src/controllers/Users.js";
+import { Login } from "./src/controllers/Login.js";
 import bodyParser from "body-parser";
-import auth from './src/auth/auth.js'
-import esm from "esm"
+import auth from "./src/auth/auth.js";
+import esm from "esm";
+import cors from "cors";
 
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose
-  .connect("mongodb+srv://devuser:YNOV1234@cluster0.mlauqjx.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+    .connect(
+        "mongodb+srv://devuser:YNOV1234@cluster0.mlauqjx.mongodb.net/?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 
-app.get("/articles", auth , Articles.getAll);
+app.get("/articles", auth, Articles.getAll);
 app.get("/articles/:id", auth, Articles.getOne);
-app.get("/users", auth , Users.getAll);
-app.get("/users/:id",auth , Users.getOne);
-app.get("/categories", auth , Categories.getAll);
-app.get("/categories/:id", auth , Categories.getOne);
-app.post('/api/login', Login.login);
-
-
+app.get("/users", auth, Users.getAll);
+app.get("/users/:id", auth, Users.getOne);
+app.get("/categories", auth, Categories.getAll);
+app.get("/categories/:id", auth, Categories.getOne);
+app.post("/api/login", Login.login);
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
--- back/src/controllers/Login.js
+++ back/src/controllers/Login.js
@@ -1,38 +1,40 @@
-
-import jwt from 'jsonwebtoken'
-import {CUSTOM_PRIVATE_KEY} from '../auth/private_key.js';
-import User from '../models/User.js';
+import jwt from "jsonwebtoken";
+import { CUSTOM_PRIVATE_KEY } from "../auth/private_key.js";
+import User from "../models/User.js";
 
 export const Login = {
     login: async (req, res) => {
-      
-        User.findOne({ where: { pseudo: req.body.username } })
+        User.findOne({ pseudo: req.body.username })
             .then((user) => {
                 if (!user) {
-                    const message = "L'utilisateur demandé n'existe pas."
-                    return res.status(404).json({ message })
+                    const message = "L'utilisateur demandé n'existe pas.";
+                    return res.status(404).json({ message });
                 }
-                console.log(user)
-                if (req.body.password !== user.password) {
-                    const message = 'Le mot de passe est incorrect'
-                    return res.status(401).json({ message })
+                console.log(user.pseudo);
+                console.log(req.body.username);
 
+                if (
+                    req.body.password === user.password &&
+                    user.pseudo === req.body.username
+                ) {
+                    console.log("Ok");
+                    const token = jwt.sign(
+                        { userId: user.id },
+                        CUSTOM_PRIVATE_KEY,
+                        { expiresIn: "24h" }
+                    );
+
+                    const message = "L'utilisateur a été connecté avec succès";
+                    return res.status(200).json({ message, data: user, token });
+                } else {
+                    const message = "Le mot de passe est incorrect";
+                    return res.status(401).json({ message });
                 }
-    // JWT
-                const token = jwt.sign(
-                 { userId: user.id },
-                 CUSTOM_PRIVATE_KEY,
-                { expiresIn: '24h' }
-                )
-      
-                const message = `L'utilisateur a été connecté avec succès`
-                return res.json({ message, data: user, token })
             })
             .catch((error) => {
                 const message =
-                    "L'utilisateur n'as pas pu se connecter. Réessayez dans quelques instants."
-                return res.json({ message, data: error })
-            })
-   
-}
-}
+                    "L'utilisateur n'as pas pu se connecter. Réessayez dans quelques instants.";
+                return res.status(500).json({ message, data: error });
+            });
+    },
+};
