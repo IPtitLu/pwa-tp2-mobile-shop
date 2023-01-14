8604c629d9b53f9cd78655af078c543b82f9d660
Nephyrr <lucasperez702@gmail.com>
2023-01-14 22:47:58 +0100

    final project

--- front/front-app/src/Components/Card.js
+++ front/front-app/src/Components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ watchInfo }) => {
     return (
         <li
             key={watchInfo._id}
-            className="bg-white mx-4 flex items-center justify-end flex-col w-1/4 mt-5 px-4 card-item rounded-custom drop-shadow-xl hover:scale-105  ease-in-out duration-200
+            className="bg-white mx-4 flex items-center justify-start flex-col w-1/4 mt-5 card-item rounded-custom drop-shadow-xl hover:scale-105  ease-in-out duration-200
 
 
         "
@@ -14,11 +14,11 @@ const Card = ({ watchInfo }) => {
                 <img
                     src={watchInfo.image}
                     alt="watch"
-                    className="w-full max-h-22 object-cover"
+                    className="w-full h-22 object-cover card-item-img"
                 />
-                <h3 className="my-2 font-medium">{watchInfo.nom}</h3>
-                <p className="mt-2 mb-4 font-bold text-orange">
-                    {watchInfo.prix}
+                <h3 className="my-2 font-medium px-4">{watchInfo.nom}</h3>
+                <p className="mt-2 mb-4 font-bold text-orange px-4">
+                    {watchInfo.prix} €
                 </p>
             </Link>
         </li>
--- front/front-app/src/Pages/Product.js
+++ front/front-app/src/Pages/Product.js
@@ -4,18 +4,23 @@ import { useParams } from "react-router-dom";
 import WatchesData from "../Components/WatchesData";
 import { FaArrowLeft } from "react-icons/fa";
 import axios from "axios";
-
+import { useNavigate } from "react-router-dom";
 
 const Product = () => {
     const [state, setState] = useState([]);
-    const [currentUser, setCurrentUser] = useState([])
+    const [currentUser, setCurrentUser] = useState([]);
     const params = useParams();
+    const loremIpsum =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    const navigate = useNavigate();
 
     const watchId = params.id;
     useEffect(() => {
-        console.log("letsgo")
+        console.log("letsgo");
         async function fetchData() {
-            const response = await fetch(`http://localhost:3000/articles/${watchId}`);
+            const response = await fetch(
+                `http://localhost:3000/articles/${watchId}`
+            );
             const data = await response.json();
             console.log(data);
             setState(data);
@@ -23,64 +28,75 @@ const Product = () => {
         fetchData();
     }, []);
 
-    useEffect(()=> {
+    useEffect(() => {
         async function getUser() {
-            
-            const userData =  localStorage.getItem("user");
-            const response = await fetch(`http://localhost:3000/users_by_username/${userData}`);
+            const userData = localStorage.getItem("user");
+            const response = await fetch(
+                `http://localhost:3000/users_by_username/${userData}`
+            );
             const data = await response.json();
             console.log(data);
             setCurrentUser(data);
-        
         }
         getUser();
     }, [state]);
 
     function addToCart() {
-        return axios.put(`http://localhost:3000/add-article/${currentUser._id}/${state._id}`)
-        .then()
-        .catch(error => {
-          console.log(error);
-          throw error;
-        });
-
+        return axios
+            .put(
+                `http://localhost:3000/add-article/${currentUser._id}/${state._id}`
+            )
+            .then(
+                setTimeout(() => {
+                    navigate("/cart");
+                }, 1000)
+            )
+            .catch((error) => {
+                console.log(error);
+                throw error;
+            });
     }
-
-    
 
     return (
         <div className="w-full flex justify-center bg-white max-h-screen">
-           { state ? 
-           <div className="w-full max-w-md relative max-h-screen bg-card-gray mx-2 flex items-center flex-col">
-                <a href="/home" className="w-full mt-10 ml-4 pl-4 button">
-                    <FaArrowLeft className="w-6 h-6 border-0 border-orange hover:color-orange hover:border-b-4 h-19 ease-in-out duration-100" />
-                </a>
-                <img
-                    src={state.image}
-                    alt="watch"
-                    className="w-2/4 max-h-22 object-cover mt-12 mb-8"
-                />
-                <div className="ml-4 mr-4">
-                    <h3 className="text-3xl font-semibold text-ledt my-4">
-                        {state.nom}
-                    </h3>
-                    <p className="text-2xl font-bold text-orange text-left my-2">
-                        {state.prix} €
-                    </p>
-                    <p className="font-medium text-left mb-2 mt-10">About</p>
-                    <p className="font-light text-left my-2">aaa</p>
-                    <button onClick={addToCart}
-                        className="w-full h-max px-4 py-3 bg-orange text-white mx-2 mt-8 mb-5 categorie-item font-semibold drop-shadow-lg hover:bg-white hover:text-orange ease-in-out duration-200
+            {state ? (
+                <div className="w-full max-w-md relative max-h-screen bg-card-gray mx-2 flex items-center flex-col">
+                    <a href="/home" className="w-full mt-10 ml-4 pl-4 button">
+                        <FaArrowLeft className="w-6 h-6 border-0 border-orange hover:color-orange hover:border-b-4 h-19 ease-in-out duration-100" />
+                    </a>
+                    <img
+                        src={state.image}
+                        alt="watch"
+                        className="w-2/4 max-h-22 object-cover mt-12 mb-8"
+                    />
+                    <div className="ml-4 mr-4 card-detail">
+                        <h3 className="text-3xl font-semibold text-ledt my-4">
+                            {state.nom}
+                        </h3>
+                        <p className="text-2xl font-bold text-orange text-left my-2">
+                            {state.prix} €
+                        </p>
+                        <p className="font-medium text-left mb-2 mt-10">
+                            About
+                        </p>
+                        <p className="font-light text-center my-2">
+                            {loremIpsum}
+                        </p>
+                        <button
+                            onClick={addToCart}
+                            className="w-full h-max px-4 py-3 bg-orange text-white mx-2 mt-8 mb-5 categorie-item font-semibold drop-shadow-lg hover:bg-white hover:text-orange ease-in-out duration-200
 
                 "
-                    >
-                        Add to cart
-                    </button>
-                </div>
+                        >
+                            Add to cart
+                        </button>
+                    </div>
 
-                <Footer />
-            </div>              : <div>Loading...</div>
-} 
+                    <Footer />
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
         </div>
     );
 };
