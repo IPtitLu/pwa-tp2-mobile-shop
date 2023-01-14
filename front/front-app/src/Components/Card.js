79234a75383af48f5480974a846ad9d28f6a7a99
hugoty <hugo.raoult@hotmail.fr>
2023-01-14 19:26:54 +0100

    affichage list/articles

--- front/front-app/src/Components/Card.js
+++ front/front-app/src/Components/Card.js
@@ -4,21 +4,21 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 const Card = ({ watchInfo }) => {
     return (
         <li
-            key={watchInfo.id}
+            key={watchInfo._id}
             className="bg-white mx-4 flex items-center justify-end flex-col w-1/4 mt-5 px-4 card-item rounded-custom drop-shadow-xl hover:scale-105  ease-in-out duration-200
 
 
         "
         >
-            <Link to={`/produit/${watchInfo.id}`}>
+            <Link to={`/produit/${watchInfo._id}`}>
                 <img
                     src={watchInfo.image}
                     alt="watch"
                     className="w-full max-h-22 object-cover"
                 />
-                <h3 className="my-2 font-medium">{watchInfo.name}</h3>
+                <h3 className="my-2 font-medium">{watchInfo.nom}</h3>
                 <p className="mt-2 mb-4 font-bold text-orange">
-                    {watchInfo.price}
+                    {watchInfo.prix}
                 </p>
             </Link>
         </li>
--- front/front-app/src/Pages/Product.js
+++ front/front-app/src/Pages/Product.js
@@ -2,34 +2,45 @@ import { React, useState, useEffect } from "react";
 import Footer from "../Components/Footer";
 import { useParams } from "react-router-dom";
 import WatchesData from "../Components/WatchesData";
-import watchImg from "../Assets/watch.png";
 import { FaArrowLeft } from "react-icons/fa";
 
 const Product = () => {
+    const [state, setState] = useState();
     const params = useParams();
 
-    const watch = WatchesData.find((prod) => prod.id == params.id);
+    const watchId = params.id;
+    useEffect(() => {
+        console.log("letsgo")
+        async function fetchData() {
+            const response = await fetch(`http://localhost:3000/articles/${watchId}`);
+            const data = await response.json();
+            console.log(data);
+            setState(data);
+        }
+        fetchData();
+    }, []);
 
     return (
         <div className="w-full flex justify-center bg-white max-h-screen">
-            <div className="w-full max-w-md relative max-h-screen bg-card-gray mx-2 flex items-center flex-col">
+           { state ? 
+           <div className="w-full max-w-md relative max-h-screen bg-card-gray mx-2 flex items-center flex-col">
                 <a href="/home" className="w-full mt-10 ml-4 pl-4 button">
                     <FaArrowLeft className="w-6 h-6 border-0 border-orange hover:color-orange hover:border-b-4 h-19 ease-in-out duration-100" />
                 </a>
                 <img
-                    src={watchImg}
+                    src={state.image}
                     alt="watch"
                     className="w-2/4 max-h-22 object-cover mt-12 mb-8"
                 />
                 <div className="ml-4 mr-4">
                     <h3 className="text-3xl font-semibold text-ledt my-4">
-                        {watch.name}
+                        {state.nom}
                     </h3>
                     <p className="text-2xl font-bold text-orange text-left my-2">
-                        {watch.price}
+                        {state.prix} €
                     </p>
                     <p className="font-medium text-left mb-2 mt-10">About</p>
-                    <p className="font-light text-left my-2">{watch.about}</p>
+                    <p className="font-light text-left my-2">aaa</p>
                     <button
                         className="w-full h-max px-4 py-3 bg-orange text-white mx-2 mt-8 mb-5 categorie-item font-semibold drop-shadow-lg hover:bg-white hover:text-orange ease-in-out duration-200
 
@@ -40,7 +51,8 @@ const Product = () => {
                 </div>
 
                 <Footer />
-            </div>
+            </div>              : <div>Loading...</div>
+} 
         </div>
     );
 };
